3922f24d12b2e9e1cb470c717096495365582076
罗彦昊 <yanhaoluo99783@163.com>
2022-11-14 23:37:47 +0800

    2022年11月14日 工厂模式further：添加注释

--- Java/com/java/designpatterns/src/main/java/org/example/factoryPattern/further/Main.java
+++ Java/com/java/designpatterns/src/main/java/org/example/factoryPattern/further/Main.java
@@ -1,5 +1,13 @@
 package org.example.factoryPattern.further;
 
+/**
+ * 工厂模式
+ * ①创建一个通用接口，然后多方法实现该通用接口
+ * ②创建工厂类（该类用于匹配具体的内容），但是返回接口
+ * ③使用：创建该工厂，再调用具体方法时为相关实现方案
+ *
+ * 根据不同类型返回对应的处理方法
+ */
 public class Main {
     public static void main(String[] args) {
         String type = "1";
